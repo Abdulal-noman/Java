9e0f6381dcf8a897aa5728a4920afc7e1e2f7bef
Abdulal-noman <mdroman45678910@gmail.com>
2025-02-16 10:35:40 +0000

    this is the first single linksit in java add most comment for batter undasting

--- OOps/LL/singleLL.java
+++ OOps/LL/singleLL.java
@@ -1,37 +1,59 @@
 public class singleLL {
-    
-     class node{
+
+    class node {// create a class
         int data;
         node next;
 
-        node(int data){
-            this.data=data;
-            this.next=null;
+        node(int data) {
+            this.data = data;
+            this.next = null;
         }
 
-    }   
-    node head=null;
-    // method to add a not at he end
-    public void add(int data){
-        node newnode=new node(data);
-        if(head==null){
-            head=newnode;
-
-        }else{
-            node temp=head;
-            while (temp.next!=null) {
-                temp=temp.next;
-
-            }
-            temp.next=newnode;
-            
-        }
     }
 
-    
+    node head = null;
+    node tail = null;// add locate the data into null for all
+
+    // add node()
+    // when we add a new data this function halp crate a new data;
+
+    public void addnode(int data) {
+        node first = new node(data);// this the new node
+        if (head == null) {
+            // when the ll has only a single list it has
+            // only one node that why tali and head are same
+            head = first;
+            tail = first;
+
+        } else {
+            tail.next = first;// first determine the new node
+            tail = first;// it is determine the new node tail
+        }
+
+    }
+
+    // print the linked list
+
+    public void pirnt() {
+        // sellect a node as curreent node
+        // this node connect the next node
+        // this node runing untill null of the node
+        node current = head;// currrrent node determin as a head node
+
+        while (current != null) {
+            System.out.print(current.data + "   ->  ");
+            current = current.next;
+
+        }
+        System.out.print("null");
+    }
+
     public static void main(String[] args) {
-        singleLL list =new singleLL();
-        
-        
+        singleLL list = new singleLL();
+        list.addnode(0);
+        list.addnode(1);
+        list.addnode(2);
+        list.pirnt();
+
     }
 }
