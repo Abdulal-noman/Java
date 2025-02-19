afdcab3999ec2256a6502ad953a2a91fa89ea664
Abdulal-noman <mdroman45678910@gmail.com>
2025-02-19 18:44:25 +0000

    nothing but basics start

---
+++ OOps/Tree/basicsTree.java
@@ -0,0 +1,18 @@
+public class basicsTree {
+    public class node {
+    
+        int value;
+        Node left,right;
+
+        public Node(int value){
+            this.value=value;
+            left=right=null;
+        }
+    }
+    public BinaryTree {
+        Node root;
+        
+    
+        
+    }
+}
